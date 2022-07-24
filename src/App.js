92a9b91cda88ab91ada5aa909e16156bf34d7e3a
MistbornL <lashaloria3@gmail.com>
2022-07-24 18:12:52 +0400

    update

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.scss";
+import { Tips } from "./data/dummydata";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          SPLI
+          <br />
+          TTER
+        </h1>
+      </header>
+      <main>
+        <div className="calculator">
+          <div className="calculator-left-top">
+            <p>Bill</p>
+            <input type="number" placeholder="0"></input>
+          </div>
+
+          <div className="calculator-left-middle">
+            <p>Selected Tip %</p>
+            <div className="tip-buttons">
+              {Tips.map((data) => {
+                return <button>{data.value}</button>;
+              })}
+            </div>
+          </div>
+
+          <div className="calculator-left-bottom"></div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/dummydata.js
@@ -0,0 +1,7 @@
+export const Tips = [
+  { id: 1, value: 5 },
+  { id: 1, value: 10 },
+  { id: 1, value: 15 },
+  { id: 1, value: 25 },
+  { id: 1, value: 50 },
+];
