ff12d228be1fe509acafce496b661b638b750a8e
MistbornL <lashaloria3@gmail.com>
2022-07-24 18:49:52 +0400

    left styling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <main>
         <div className="calculator">
           <div className="calculator-left">
-            <div className="calculator-left-top">
+            <div className="calculator-left-top-bottom">
               <p>Bill</p>
               <input type="number" placeholder="0"></input>
             </div>
@@ -27,9 +27,28 @@ function App() {
               </div>
             </div>
 
-            <div className="calculator-left-bottom">
+            <div className="calculator-left-top-bottom">
               <p>Number of People</p>
               <input type="number" placeholder="0"></input>
+            </div>
+          </div>
+
+          <div className="calculator-right">
+            <div className="calculator-right-top">
+              <div className="render-amount">
+                <h2>
+                  Tip Amount <br />
+                  <span>/ Person</span>
+                </h2>
+                <span>$0.00</span>
+              </div>
+              <div className="render-amount">
+                <h2>
+                  Total Amount <br />
+                  <span>/ Person</span>
+                  <span>$0.00</span>
+                </h2>
+              </div>
             </div>
           </div>
         </div>
